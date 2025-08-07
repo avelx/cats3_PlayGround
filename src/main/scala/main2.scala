677d65a1e692d4c09c10d89ba7b9c4fe79695f3c
Pavel <pavel.oliynyk@gmail.com>
2025-08-07 15:33:39 +0100

    feat: cats-effect :: ASync

--- src/main/scala/main2.scala
+++ src/main/scala/main2.scala
@@ -1,20 +1,43 @@
-import cats.effect.{ExitCode, IO, IOApp}
+import cats.effect.{Async, ExitCode, IO, IOApp}
+import cats.implicits.catsSyntaxFlatMapOps
 
 import java.util.concurrent.{Executors, TimeUnit}
+import scala.concurrent.Future
 
 object  main2 extends IOApp {
 
   import cats.effect.IO
-  
+
   override def run(args: List[String]): IO[ExitCode] = {
-    val ioa = IO {
-      println("hey!")
+    val iob = (x: Either[Throwable, String]) => IO {
+      println(s"Hay: $x")
     }
 
+    //val scheduler = Executors.newScheduledThreadPool(1)
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    val apiCall = Future{
+      Thread.sleep(3000)
+      println("Data test")
+      "Outcome Ok"
+    }
+
+    val ioa: IO[String] = Async[IO].async { cb =>
+      import scala.util.{Failure, Success}
+
+      apiCall.onComplete {
+        case Success(value) => cb(Right(value))
+        case Failure(error) => cb(Left(error))
+      }
+    }
+
+
+//    import cats.syntax.either._
     val program: IO[ExitCode] =
       for {
-        _ <- ioa
-        _ <- ioa
+        _ <- iob(Right("String"))
+        x <- ioa
+        _ <- iob(Left(new Error("Data")))
       } yield ExitCode.Success
 
     IO.pure {
